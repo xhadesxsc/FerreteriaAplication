97e4efb3b54576ce71d5dfc1ceda402d18dbc9e0
xhadesxsc <leo_2085@hotmail.com>
2015-06-06 15:45:53 -0500

    prueba 1

---
+++ LIB_ENTIDAD/cls_producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LIB_ENTIDAD
+{
+    public class cls_producto
+    {
+        public string cod_pro;
+        public string pro_nom;
+        public string pro_des;
+        public int cod_cat;
+        public DateTime pro_fec;
+        public string pro_mar;
+        public string pro_mod;
+
+    }
+}
