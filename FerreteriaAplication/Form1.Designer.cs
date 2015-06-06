d75d8c00c180ebdb3bb5057e83ae7dbd974e87cd
xhadesxsc <leo_2085@hotmail.com>
2015-06-06 17:54:37 -0500

    form

--- FerreteriaAplication/Form1.Designer.cs
+++ FerreteriaAplication/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(793, 357);
+            this.ClientSize = new System.Drawing.Size(831, 463);
             this.Name = "Form1";
             this.Text = "Aplicacion Ferreteria";
             this.ResumeLayout(false);
